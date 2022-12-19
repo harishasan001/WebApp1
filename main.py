f2526b9d4abeb929cf5513d63de48d79fc4d4c3b
Haris Hasan <77868290+harishasan001@users.noreply.github.com>
2022-12-19 09:54:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from pandas import Series,DataFrame
+import numpy as np
+import streamlit as st
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+%matplotlib inline
+
+from pandas_datareader import DataReader
+from datetime import datetime
+
+tech_list = ['COIN','MSTR','NVDA','MARA', 'SQ', 'SI', 'RIOT', 'AMD', 'PYPL', 'CME']
+
+#Setting the end date to today
+end = datetime.now()
+
+#Start date set to 2 year back
+start = datetime(end.year-2,end.month,end.day)
+
+#Using Yahoo Finance to grab the stock data
+for stock in tech_list:
+    globals()[stock] = DataReader(stock,'yahoo',start,end)
+    
+#testing if the global variable worked 
+COIN.head()
