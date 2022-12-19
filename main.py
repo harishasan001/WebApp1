72ba9475f536a5fac2e1fe144d26dee29bbdbec6
Haris Hasan <77868290+harishasan001@users.noreply.github.com>
2022-12-19 12:49:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import seaborn as sns
 
 from pandas_datareader import DataReader, data
 from datetime import datetime, timedelta
+import yfinance as yf
 
 st.title('Stock EDA')
 st.header('Target')
@@ -17,22 +18,14 @@ st.write('The target of this EDA is to draw conclusions about the relationship b
 stock_name = st.text_input("Enter the stock name: \n")
 option = st.slider("How many days of data would you like to see?", 1,60,1)
 
-
 end = datetime.today().strftime('%Y-%m-%d')
 start = (datetime.today() - timedelta(option)).strftime('%Y-%m-%d')
-@st.cache
-def load_data(stock, start_date, end_date):
-  df = data.DataReader(name=stock, start=start_date, end=end_date, data_source='yahoo')
-  return df                                                        
 
-data_load_state = st.text("Loading data...")
-df = load_data(stock=stock_name, start_date=start, end_date=end)
-df.sort_index(axis=0, inplace=True, ascending=False)
-st.subheader(f' {stock_name} stock prices for the past {option} days')
-st.dataframe(df)
+get_stock_data = yf.Ticker(stock)
 
-chart_data = df[['Close']]                       
-st.subheader("Close Prices")
-st.line_chart(chart_data)
-data_load_state.text("Data loaded!")                                                        
-                                            
+# Set the time line of your data
+ticket_df = get_stock_data.history(period='1d', start=start, end=end)
+
+# Show your data in line chart
+st.line_chart(ticket_df.Close)
+st.line_chart(ticket_df.Volume)
